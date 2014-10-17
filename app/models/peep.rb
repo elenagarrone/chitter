1675ba3cc448e9fe3044881a59de1b60dc7beb99
elena <elena.15@hotmail.it>
2014-10-17 17:56:59 +0100

    added name to peep

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -2,12 +2,13 @@ class Peep
 
 	include  DataMapper::Resource
 
+	belongs_to :user
+
 	property :id, Serial
 	property :message, Text
 	property :date, Text, :default => (Time.now.strftime("%H:%M:%S - %d/%m/%Y"))
 
 	validates_length_of :message, :in => (1..200)
 
-	has 1, :user, :through => Resource
 
 end
--- app/server.rb
+++ app/server.rb
@@ -16,9 +16,10 @@ end
 
 post '/peeps' do
 	@user = User.first(:id => session[:user_id])
-	if @user 
+	if @user
 		peep = params["peep"]
-		Peep.create(:message => peep)
+		Peep.create(:message => peep, :user_id => @user.id)
+		# @peep = @user.peeps.create(message: peep)
 		redirect to('/')
 	else
 		flash[:errors] = ["You need to sign in to post a peep"]
