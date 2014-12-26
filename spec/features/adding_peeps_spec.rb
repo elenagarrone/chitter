c1097d9514b8c127e087056baf8884b6a6e29462
Elena <elena.15@hotmail.it>
2014-12-26 15:43:38 +0000

    remove file chitter.html

--- spec/features/adding_peeps_spec.rb
+++ spec/features/adding_peeps_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 require_relative 'helpers/session'
 
+include SessionHelpers
+
 feature "User post a new peep" do
 
 	before(:each) do
@@ -11,24 +13,22 @@ feature "User post a new peep" do
 		:password_confirmation => "elena1")
 	end
 
-	feature "when browsing the homepage" do
+	scenario "when browsing the homepage" do
+		sign_in('elena15', 'elena1')
+		expect(Peep.count).to eq(0)
+		visit '/'
+		add_peep("Hi everyone!")
+		expect(Peep.count).to eq(1)
+		peep = Peep.first
+		expect(peep.message).to eq("Hi everyone!")
+	end
 
-		scenario "while signed in" do
-			sign_in('elena15', 'elena1')
-			expect(Peep.count).to eq(0)
+	scenario "only when logged in" do
 			visit '/'
-			add_peep("Hi everyone!")
-			expect(Peep.count).to eq(1)
-			peep = Peep.first
-			expect(peep.message).to eq("Hi everyone!")
-		end
-
-		scenario "cannot add a peep if not logged in" do
-				visit '/'
-				expect(page).not_to have_field('#new_peep')
-				expect(page).not_to have_button('Add peep')
-		end
-
+			expect(page).not_to have_field('#new_peep')
+			expect(page).not_to have_button('Add peep')
+			sign_in('elena15', 'elena1')
+			expect(page).to have_button('Add peep')
 	end
 
 end
--- spec/features/buttons_view_spec.rb
+++ spec/features/buttons_view_spec.rb
@@ -3,7 +3,7 @@ require_relative 'helpers/session'
 
 include SessionHelpers
 
-feature "I should see these buttons: " do
+feature "Buttons: " do
 
   before(:each) do
     User.create(:name => "Elena Garrone",
@@ -11,12 +11,12 @@ feature "I should see these buttons: " do
     :email => "elena@example.com",
     :password => "elena1",
     :password_confirmation => "elena1")
+    sign_in('elena15', 'elena1')
   end
 
   context "on the homepage" do
 
     scenario "when signed in i should see 'Sign out'" do
-      sign_in('elena15', 'elena1')
       visit '/'
       expect(page).to have_button('Sign out')
       expect(page).not_to have_link('Sign in')
@@ -24,10 +24,20 @@ feature "I should see these buttons: " do
     end
 
     scenario "when signed out I should see 'Sign in' and 'Sign out'" do
+      sign_out
       visit '/'
       expect(page).to have_link('Sign in')
       expect(page).to have_link('Sign up')
       expect(page).not_to have_button('Sign out')
+    end
+
+  end
+
+  context "on the sign up page" do
+
+    scenario "I should see 'sign in'" do
+      visit '/users/new'
+      expect(page).to have_link('Sign in')
     end
 
   end
